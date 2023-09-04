3936164a49478b6211c6a9480b52f6fabd6874c8
Angel Villanueva <74369085+AngelVillanueva28@users.noreply.github.com>
2023-09-03 20:23:26 -0500

    Update server.py
    Signed-off-by: Angel Villanueva <74369085+AngelVillanueva28@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,21 +1,59 @@
 import os
-from flask import Flask, send_from_directory, render_template, redirect
+from flask import Flask, request, jsonify
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from collections import Counter
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
 
 app = Flask(__name__)
 
-port = int(os.environ.get("PORT", 5000))
+def preprocess_text(text):
+    tokens = word_tokenize(text)
+    tokens = [word.lower() for word in tokens if word.isalpha()]
+    tokens = [word for word in tokens if word not in stopwords.words('english')]
+    return tokens
 
-@app.route('/static/<path:path>')
-def serve_static(path):
-    return send_from_directory('static', path)
+def calculate_match_percentage(cv_tokens, job_tokens):
+    cv_word_count = Counter(cv_tokens)
+    job_word_count = Counter(job_tokens)
 
-@app.route('/')
-def home():
-   return render_template('index.html')
+    total_keywords = len(job_word_count)
+    
+    if total_keywords == 0:
+        return 0  # Avoid division by zero
 
-@app.route('/<path:path>')
-def all_routes(path):
-    return redirect('/')
+    matching_keywords = sum(min(cv_word_count[key], job_word_count[key]) for key in job_word_count.keys())
 
-if __name__ == "__main__":
-    app.run(port=port)
+    match_percentage = (matching_keywords / total_keywords) * 100
+
+    return match_percentage
+
+@app.route('/', methods=['POST'])
+def calculate_matches():
+    try:
+        data = request.get_json()
+        cvs = data['cvs']
+        job_description = data['Oportunidad_Laboral']
+        
+        results = []
+        
+        job_tokens = preprocess_text(job_description)
+        
+        for cv in cvs:
+            cv_text = cv['CV']
+            cv_tokens = preprocess_text(cv_text)
+            match_percentage = calculate_match_percentage(cv_tokens, job_tokens)
+            results.append({
+                'id_candidato': cv['id_candidato'],
+                'match_percentage': match_percentage
+            })
+        
+        return jsonify(results)
+    except KeyError:
+        return jsonify({'error': 'Invalid JSON data'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=False)
